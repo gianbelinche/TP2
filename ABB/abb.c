113784e07d3a2170fd77d8694df3eac63f6642c6
gianbelinche <gianbelinche@hotmail.com.ar>
2018-10-24 19:10:02 -0300

    DEfinicion struct abb

--- ABB/abb.c
+++ ABB/abb.c
@@ -1,1 +1,14 @@
 //Este va a ser el abb
+typedef int (*abb_comparar_clave_t) (const char *, const char *);
+typedef void (*abb_destruir_dato_t) (void *);
+
+typedef struct abb{
+	struct abb* izq;
+	struct abb* der;
+	char* clave;
+	void* dato;
+	size_t cantidad;
+	abb_comparar_clave_t cmp;
+	abb_destruir_dato_t destruir_dato;
+}abb_t;
+
