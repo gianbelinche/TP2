5f53e26dde53e270d131ae7ee22411d0bff9f51a
Franco <francobarreneche@hotmail.com.ar>
2018-10-25 21:23:39 -0300

    Puse cosas mías y las integre a lo tuyo, compila pero se rompe

--- ABB/abb.c
+++ ABB/abb.c
@@ -1,4 +1,19 @@
 //Este va a ser el abb
+//Bueno
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#include "abb.h"
+// -_-_-_-_-_-_-_-_-_-_-_      CONSTANTES     -_-_-_-_-_-_-_-_-_-_-_- //
+#define RAIZ 0
+#define IZQ  0
+#define DER  1
+
+// -_-_-_-_-_-_-_-_  DEFINICION DE  TIPOS DE DATO  _-_-_-_-_-_-_-_-_- //
+
+
 typedef int (*abb_comparar_clave_t) (const char *, const char *);
 typedef void (*abb_destruir_dato_t) (void *);
 
@@ -12,25 +27,41 @@ typedef struct abb{
 	abb_destruir_dato_t destruir_dato;
 }abb_t;
 
-void abb_destruir(abb_t* abb){
-	if (!abb) return;
-	abb_destruir(abb->izq);
-	abb_destruir(abb->der);
-	free(abb->clave);
-	if (abb->destruir_dato)
-		abb->destruir_dato(abb->dato);
-	free(abb);
+// -_-_-_-_-_-_-_-_-_-_-  FUNCIONES AUXILIARES  -_-_-_-_-_-_-_-_-_-_- //
+
+abb_t* _abb_buscar(abb_t* arbol,const char* clave,int* vinculo)
+{
+	if(!arbol) return arbol;
+
+	int comparacion = arbol -> cmp(arbol -> clave,clave);
+
+	if (comparacion == 0) 
+		return arbol;
+	
+	if (comparacion > 0)
+	{
+		*vinculo = DER;
+		return _abb_buscar(arbol -> der,clave,vinculo);
+	}
+
+	*vinculo = IZQ;
+	return _abb_buscar(arbol -> izq,clave,vinculo);
 }
 
-bool abb_pertenece(const abb_t* abb,const char* clave){
-	abb_t* arbol = abb_buscar(abb,clave);
-	return !arbol==NULL;
+abb_t* abb_buscar(const abb_t* arbol,const char* clave,int* vinculo)
+{
+	*vinculo = RAIZ;
+	return _abb_buscar(arbol -> izq,clave,vinculo);
 }
 
-void* abb_obtener(const abb_t* abb,const char* clave){
-	abb_t* arbol = abb_buscar(abb,clave);
-	if (!arbol) return NULL;
-	return arbol->dato;
+size_t abb_cantidad(abb_t *arbol)
+{
+	return arbol -> cantidad;
+}
+
+abb_t* abb_obtener_padre(abb_t* arbol,int vinculo)
+{
+	return arbol - vinculo;
 }
 
 void abb_borrar_sin_hijos(abb_t* padre,abb_t* hijo){
@@ -67,11 +98,53 @@ void abb_borrar_dos_hijos(abb_t* arbol){
 	arbol->dato = dato; 
 }
 
+// -_-_-_-_-_-_-_-_-_-_-  PRIMITIVAS DEL ABB  -_-_-_-_-_-_-_-_-_-_- //
+
+abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato)
+{
+	abb_t* arbol = calloc(1, sizeof(abb_t));
+	if(!arbol) return NULL;
+
+	arbol -> cmp = cmp;
+	arbol -> destruir_dato = destruir_dato;
+	return arbol;
+}
+
+bool abb_guardar(abb_t *arbol, const char *clave, void *dato)
+{
+	int vinculo;
+	abb_t* hijo = abb_buscar(arbol,clave,&vinculo);
+
+	if(!hijo)
+	{
+		hijo = abb_crear(arbol -> cmp,arbol -> destruir_dato);
+		if(!hijo) return false;
+	}
+	
+	hijo -> dato = dato;
+	return true;
+}
+
+bool abb_pertenece(const abb_t* abb,const char* clave){
+	int vinculo;
+	abb_t* arbol = abb_buscar(abb,clave,&vinculo);
+	return arbol;
+}
+
+void* abb_obtener(const abb_t* abb,const char* clave){
+	int vinculo;
+	abb_t* arbol = abb_buscar(abb,clave,&vinculo);
+	if (!arbol) return NULL;
+	return arbol->dato;
+}
+
+
 void* abb_borrar(abb_t* abb,const char* clave){
-	abb_t* arbol = abb_buscar(abb,clave);
+	int vinculo;
+	abb_t* arbol = abb_buscar(abb,clave,&vinculo);
 	if (!arbol) return NULL;
 	void* dato = arbol->dato;
-	abb_t* padre = abb_obtener_padre(abb,clave);
+	abb_t* padre = abb_obtener_padre(abb,vinculo);
 	if (!arbol->izq && !arbol->der)
 		abb_borrar_sin_hijos(padre,arbol);
 	else if (arbol->izq || arbol->der)
@@ -80,3 +153,15 @@ void* abb_borrar(abb_t* abb,const char* clave){
 		abb_borrar_dos_hijos(arbol);
 	return dato;
 }
+
+void abb_destruir(abb_t* abb){
+	if (!abb) return;
+	abb_destruir(abb->izq);
+	abb_destruir(abb->der);
+	free(abb->clave);
+	if (abb->destruir_dato)
+		abb->destruir_dato(abb->dato);
+	free(abb);
+}
+
+// -_-_-_-_-_-_-_-_-_-_-  ITERADOR DEL ABB  -_-_-_-_-_-_-_-_-_-_- //
