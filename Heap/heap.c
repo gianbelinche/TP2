78b8010e0d9aec8d371b7d7e187afd33766b7c95
gianbelinche <gianbelinche@hotmail.com.ar>
2018-10-30 12:30:06 -0300

    Heap ver max

--- Heap/heap.c
+++ Heap/heap.c
@@ -32,11 +32,25 @@ bool redimensionar(heap_t* heap){
 
 heap_t* heap_crear_arr(void* arreglo[], size_t n, cmp_func_t cmp){
 	heap_t* heap = malloc(sizeof(heap_t));
+	if (!heap) return NULL;
+	void** elementos = malloc(sizeof(void*) * n);
+	if (!elementos){
+		free(heap);
+		return NULL;
+	}
+	for (int i = 0;i<n;i++)
+		elementos[i] = arreglo[i];
+
 	for (int i = n-1;i >= 0;i--)
-		downheap(arreglo,i);
-	heap -> elementos = arreglo;
+		downheap(elementos,i);
+
+	heap -> elementos = elementos;
 	heap -> cantidad = n;
 	heap -> cmp = cmp;
 	return heap;
 }
 
+void* heap_ver_max(const heap_t *heap){
+	if (!heap || heap->cantidad = 0) return NULL;
+	return heap -> elementos[0];
+}
