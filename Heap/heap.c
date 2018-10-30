f0c95c9825faa784be9766fe54739a333eca9cbf
gianbelinche <gianbelinche@hotmail.com.ar>
2018-10-29 22:47:50 -0300

    Heap crear y redimensionar

--- Heap/heap.c
+++ Heap/heap.c
@@ -9,3 +9,24 @@ typedef struct heap{
 	cmp_func_t cmp;
 } heap_t;
 
+heap_t* heap_crear(cmp_func_t cmp){
+	heap_t* heap = malloc(sizeof(heap_t));
+	if (!heap) return NULL;
+	void** elementos = malloc(sizeof(void*) * TAM_INICIAL);
+	if (!elementos){
+		free(heap);
+		return NULL;
+	}
+	heap -> elementos = elementos;
+	heap -> cantidad = 0;
+	heap -> cmp = cmp;
+	return heap;
+}
+
+bool redimensionar(heap_t* heap){
+	void** nuevos_elem = realloc(heap->elementos,FACTOR_RED);
+	if (!nuevos_elem) return false;
+	heap->elementos = nuevos_elem;
+	return true;
+}
+
