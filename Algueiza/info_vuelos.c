526aba88e152ef1443dea5f35532a011fc2c8636
gianbelinche <gianbelinche@hotmail.com.ar>
2018-11-12 01:15:56 -0300

    prioridad vuelos beta 1.3

--- Algueiza/info_vuelos.c
+++ Algueiza/info_vuelos.c
@@ -1,4 +1,10 @@
 //Información de vuelos
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "hash.h"
+#include "heap.h"
 /*
 hash_t* guardar_info_vuelos(FILE* vuelos,size_t cantidad_vuelos){ //Despues veremos como guardamos los vuelos
 	hash_t* hash = hash_crear(NULL);
@@ -12,15 +18,24 @@ hash_t* guardar_info_vuelos(FILE* vuelos,size_t cantidad_vuelos){ //Despues vere
 	return hash;
 }
 */
+
 typedef struct vuelo_prioridad{
 	char* codigo;
 	int prioridad;
-}vp_t
+}vp_t;
+
+int cmp(const void* dato1, const void* dato2){
+	(vp_t*) dato1;
+	(vp_t*) dato2;
+	if (dato1->prioridad < dato2->prioridad) return 1; //Nuestro heap es de maximos, lo queremos de minimos
+	else if (dato1->prioridad > dato2->prioridad) return -1;
+	return -strcmp(dato1->codigo,dato2->codigo);
+} 
 
 void obtener_info_vuelos(hash_t* info_vuelos,char* vuelo){
 	char* info = hash_obtener(info_vuelos,vuelo);
 	if (!info) 
-		printf("Error en comando info_vuelo\n",stderr);
+		fprintf(stderr,"Error en comando info_vuelo\n");
 	else
 		printf("%s\n",info);
 }
@@ -41,13 +56,13 @@ void prioridad_vuelos_v1(heap_t* prioridades,int k){
 }
 
 void prioridad_vuelos_v2(vp_t** vuelos,int cant,int k){
-	heap_t* heap = heap_crear(); //Heap de minimos
+	heap_t* heap = heap_crear(cmp); //Heap de minimos
 	int i = 0;
 	for (;i<k && i<cant;i++){
 		heap_encolar(heap,vuelos[i]);
 	}
 	for (;i<cant;i++){
-		if (heap_ver_tope(heap)->prioridad < vuelos[i]->prioridad){ //O(n*log(k))
+		if (heap_ver_max(heap)->prioridad < vuelos[i]->prioridad){ //O(n*log(k))
 			heap_desencolar(heap); 
 			heap_encolar(heap,vuelos[i]);
 		}
@@ -59,7 +74,7 @@ void prioridad_vuelos_v2(vp_t** vuelos,int cant,int k){
 		flights[j] = vuelo;
 		j++;
 	}
-	for (int i = j;i >= 0;i--){
-		printf("%d - %s\n",flights[i] -> prioridad, flights[i] -> codigo );
+	for (int l = j;l >= 0;l--){
+		printf("%d - %s\n",flights[l] -> prioridad, flights[l] -> codigo );
 	}
 }
