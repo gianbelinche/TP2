a026deeed6af29cd5693721acf229e021d874445
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2018-11-21 21:49:27 -0300

    Algunos cambios leves

--- TP3/grafo.py
+++ TP3/grafo.py
@@ -7,47 +7,46 @@ class Grafo:
 		self.cantidad = 0
 
 	def agregar_vertice(self,v):
-		if v in self.datos.keys:
+		if v in self.datos:
 			return False
 		self.datos[v] = {}
 		self.cantidad += 1
 		return True
 
 	def agregar_arista(self,v,w,peso):
-		if v not in self.datos.keys or w not in self.datos.keys:
+		if v not in self.datos or w not in self.datos:
 			return False
 		self.datos[v][w] = peso
 		self.datos[w][v] = peso
 		return True
 
 	def estan_conectados(self,v,w):
-		if v not in self.datos.keys or w not in self.datos.keys:
+		if v not in self.datos or w not in self.datos:
 			return False
-		return w in self.datos[v].keys
+		return w in self.datos[v]
 		
 	def sacar_arista(self,v,w):
-		if v not in self.datos.keys or w not in self.datos.keys:
+		if v not in self.datos or w not in self.datos:
 			return False
 		self.datos[v].pop(w)
 		self.datos[w].pop(v)
 		return True
 
 	def sacar_vertice(self,v):
-		if v not in self.datos.keys:
+		if v not in self.datos:
 			return False
-		for w in self.datos[v].keys:
+		for w in self.datos[v]:
 			self.datos[w].pop(v)
 		self.datos.pop(v)
 		self.cantidad -= 1
 		return True
 
 	def adyacentes(self,v):
-		if v not in self.datos.keys:
-			return []
-		return list(self.datos[v].keys)
+		return list(self.datos.get(v,[])
+
 		
 	def ver_peso(self,v,w):
-		if v not in self.datos.keys or w not in self.datos.keys:
+		if v not in self.datos or w not in self.datos:
 			return None
 		return self.datos[v][w]
 
@@ -56,11 +55,11 @@ class Grafo:
 
 	def vertice_random(self):
 		i = randint(0,self.cantidad)
-		return list(self.datos.keys)[i]
+		return list(self.datos)[i]
 
 		
 	def __iter__ (self):
-		return _IterGrafo(self.cantidad,list(self.datos.keys))
+		return _IterGrafo(self.cantidad,list(self.datos))
 
 
 class _IterGrafo:
